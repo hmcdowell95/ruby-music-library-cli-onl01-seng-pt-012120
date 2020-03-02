f32dd05ede8aed99ca69124d6bfb6a1d17ccd050
Harley McDowell <hmcdowell95@gmail.com>
2020-03-02 20:29:19 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -108,7 +108,7 @@ class MusicLibraryController
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
-    if !(input > Song.all.size) || !(input < 1)
+    if !(input > Song.all.size) && !(input < 1)
       musicarray = Song.all.sort{|a,b| a.name <=> b.name}
       song = musicarray[input - 1]
     end
